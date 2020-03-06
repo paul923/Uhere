b9ae2f2fa76954bfe0b4010adff7ebe2848d769d
Justin Choi <yychoi@sfu.ca>
2020-03-05 22:48:24 -0800

    Facebook Login Implemented

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Button, StyleSheet, Text, View, ActivityIndicator, TouchableOpacity  } from 'react-native';
+import { StyleSheet, View, ActivityIndicator, TouchableOpacity  } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import * as GoogleSignIn from 'expo-google-sign-in';
+import * as Facebook from 'expo-facebook';
 
-import { Image } from 'react-native-elements';
+import { Image, Button, Text } from 'react-native-elements';
 
 import googleSignInImage from '../assets/images/google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png';
 
@@ -43,6 +44,32 @@ export default class LoginScreen extends React.Component {
     }
   };
 
+  signInWithFacebook = async () => {
+    try {
+      await Facebook.initializeAsync('2728370123955490');
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ['public_profile'],
+      });
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
+        this.setState({
+          user: (await response.json()).name
+        })
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -53,6 +80,11 @@ export default class LoginScreen extends React.Component {
             PlaceholderContent={<ActivityIndicator />}
           />
         </TouchableOpacity>
+        <Button
+          title="SIGN IN WITH FACEBOOK"
+          onPress={() => this.signInWithFacebook()}
+          />
+        <Text h4>Logged in as: {this.state.user}</Text>
       </View>
     )
   }
