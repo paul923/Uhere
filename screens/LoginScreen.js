c0846de18be6e36075291b6319c64ed3dd3f0ae9
Justin Choi <yychoi@sfu.ca>
2020-03-06 00:59:24 -0800

    Google Sign In / Facebook Sign In using firebase works

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -5,31 +5,47 @@ import * as WebBrowser from 'expo-web-browser';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import * as GoogleSignIn from 'expo-google-sign-in';
 import * as Facebook from 'expo-facebook';
-
+import * as firebase from 'firebase';
 import { Image, Button, Text } from 'react-native-elements';
 
 import googleSignInImage from '../assets/images/google_signin_buttons/web/1x/btn_google_signin_dark_normal_web.png';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAHzlzyQGQDqDxeDwwYs7o9rkgD4z7Tp6Y",
+  authDomain: "fluid-uhere.firebaseapp.com",
+  databaseURL: "https://fluid-uhere.firebaseio.com",
+  projectId: "fluid-uhere",
+  storageBucket: "fluid-uhere.appspot.com",
+  messagingSenderId: "605844819615",
+  appId: "1:605844819615:web:e539873a514255ba875caa",
+  measurementId: "G-XYCTQHJC1Q"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
 
 export default class LoginScreen extends React.Component {
   state = { user: null };
 
   componentDidMount() {
+    // Listen for authentication state to change.
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user != null) {
+        console.log("We are authenticated now!");
+        this.setState({
+          user: user.displayName
+        })
+      }
+    });
     this.initAsync();
+
   }
 
   initAsync = async () => {
     await GoogleSignIn.initAsync({});
-    this._syncUserWithStateAsync();
-  };
-
-  _syncUserWithStateAsync = async () => {
-    const user = await GoogleSignIn.signInSilentlyAsync();
-    this.setState({ user });
-  };
-
-  signOutAsync = async () => {
-    await GoogleSignIn.signOutAsync();
-    this.setState({ user: null });
   };
 
   signInWithGoogle = async () => {
@@ -37,7 +53,10 @@ export default class LoginScreen extends React.Component {
       await GoogleSignIn.askForPlayServicesAsync();
       const { type, user } = await GoogleSignIn.signInAsync();
       if (type === 'success') {
-        this._syncUserWithStateAsync();
+        const credential = firebase.auth.GoogleAuthProvider.credential(user.auth.idToken, user.auth.accessToken);
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          // Handle Errors here.
+        });
       }
     } catch ({ message }) {
       alert('login: Error:' + message);
@@ -57,17 +76,24 @@ export default class LoginScreen extends React.Component {
         permissions: ['public_profile'],
       });
       if (type === 'success') {
-        // Get the user's name using Facebook's Graph API
-        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-        this.setState({
-          user: (await response.json()).name
-        })
+        const credential = firebase.auth.FacebookAuthProvider.credential(token);
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          // Handle Errors here.
+        });
       } else {
         // type === 'cancel'
       }
     } catch ({ message }) {
       alert(`Facebook Login Error: ${message}`);
     }
+  }
+
+  signOut = async () => {
+    firebase.auth().signOut().then(() => {
+      this.setState({
+        user: null
+      })
+    })
   }
 
   render() {
@@ -83,6 +109,10 @@ export default class LoginScreen extends React.Component {
         <Button
           title="SIGN IN WITH FACEBOOK"
           onPress={() => this.signInWithFacebook()}
+          />
+        <Button
+          title="SIGN OUT"
+          onPress={() => this.signOut()}
           />
         <Text h4>Logged in as: {this.state.user}</Text>
       </View>
