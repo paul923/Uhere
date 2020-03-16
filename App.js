e408cf0e40f2f9473600e1ef2fc0bfc2bd07fee0
Paul Kim <paulgmkim@gmail.com>
2020-03-15 23:45:03 -0700

    feat: added slider to components directory and minor style changed

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Platform, StatusBar, StyleSheet, View, Text, Image } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -9,20 +10,36 @@ import { createStackNavigator } from '@react-navigation/stack';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
+import AppIntroSlider from './components/introSlider';
+
+
+
 const Stack = createStackNavigator();
 
-export default function App(props) {
 
-  return (
-    <View style={styles.container}>
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Root" component={BottomTabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
+export default function App(props) {
+  
+  const [showRealApp, setshowRealApp] = React.useState(false);
+
+  if(showRealApp){
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    );
+  } else {
+    return <AppIntroSlider 
+            slides={slides} 
+            onDone={() => {setshowRealApp(true);}} 
+            showSkipButton
+            activeDotStyle={{backgroundColor: 'rgba(0, 0, 0, .9)'}}
+            />;
+  }
 
 }
 
@@ -32,3 +49,47 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+
+// Slider contents
+const slides = [
+  {
+    key: 'Dummy1',
+    title: 'Dummy 1',
+    text: 'Please sign in to\ncontinue.',
+    image: require('./assets/images/robot-dev.png'),
+    backgroundColor: '#ffffff',
+    titleStyle: {
+      color: '#000000'
+    },
+    textStyle : {
+      color: '#0f0f0f',
+    }
+  },
+  {
+    key: 'Dummy2',
+    title: 'Dummy 2',
+    text: 'Please sign in to\ncontinue.',
+    image: require('./assets/images/robot-dev.png'),
+    backgroundColor: '#ffffff',
+    titleStyle: {
+      color: '#000000'
+    },
+    textStyle : {
+      color: '#0f0f0f',
+    }
+  },
+  {
+    key: 'Dummy3',
+    title: 'Dummy 3',
+    text: 'Please sign in to\ncontinue.',
+    image: require('./assets/images/robot-dev.png'),
+    backgroundColor: '#ffffff',
+    titleStyle: {
+      color: '#000000'
+    },
+    textStyle : {
+      color: '#0f0f0f'
+    }
+  }
+ ];
