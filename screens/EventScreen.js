d4559405398cb7336d255b864d52bc1f599f6b69
Justin Choi <youngyoonc@gmail.com>
2020-03-16 20:55:03 -0700

    feat(event): Created basic event styling

--- screens/EventScreen.js
+++ screens/EventScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, View, ActivityIndicator, TouchableOpacity  } from 'react-native';
-import { Image, Button, Text } from 'react-native-elements';
+import { FlatList, StyleSheet, View, ActivityIndicator, TouchableOpacity  } from 'react-native';
+import { Image, Button, Text, ListItem, Divider, Icon } from 'react-native-elements';
 import AuthContext from '../contexts/AuthContext';
 import firebaseObject from '../config/firebase';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -8,10 +8,63 @@ import { useSafeArea } from 'react-native-safe-area-context';
 const Tab = createMaterialTopTabNavigator();
 
 
+function formatDate(date){
+  return date.getMonth() + " " + date.getDate() + ", " + date.getFullYear();
+}
+
+function formatTime(date){
+  return date.getHours() + ":" + date.getMinutes();
+}
+
 function PendingEvent(){
+  renderItem = ({ item }) => (
+    <View>
+      <View style={styles.card}>
+        <Text h4>{item.name}</Text>
+        <View style={styles.row}>
+          <Icon name="event"/>
+          <View style={styles.cardColumn}>
+            <Text h5>{formatDate(item.date)}</Text>
+            <Text h5>{formatTime(item.date)}</Text>
+          </View>
+          <Icon name="remove-circle"/>
+          <View style={styles.cardColumn}>
+            <Text h5>{item.prize}</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <Icon name="location-on"/>
+          <View style={styles.cardColumn}>
+            <Text h5>{item.location}</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <Icon name="person"/>
+          <View style={styles.cardColumn}>
+            <Text h5>{item.members.length + "/" + item.maximumNumberOfMembers}</Text>
+          </View>
+        </View>
+      </View>
+      <Divider style={{ height: 0.3, margin: 5, backgroundColor: 'black' }} />
+    </View>
+  )
   return (
     <View style={styles.container}>
-      <Text>Pending Event</Text>
+      <FlatList
+        data={[
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' },
+          { name: 'GAZUA', date: new Date(), location: 'Juilet Cafe', maximumNumberOfMembers: 5, members: [], prize: 'americano' }
+        ]}
+        contentContainerStyle={{flexGrow: 1, alignItems: 'center'}}
+        renderItem={renderItem}
+        style={styles.list}
+      />
     </View>
   )
 }
@@ -53,9 +106,28 @@ export default function EventScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF'
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#F5FCFF'
+    },
+    list: {
+      width: '100%',
+    },
+    row: {
+      width: '100%',
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center'
+    },
+    cardColumn: {
+      width: '40%'
+    },
+    card: {
+      marginTop: 10,
+      marginBottom: 10,
+      width: '100%',
+      backgroundColor: '#C4C4C4'
     }
 });
