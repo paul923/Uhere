a3cbdb82eac6b3a730c2cea3d98c2f3509950aa2
Justin Choi <youngyoonc@gmail.com>
2020-03-18 23:52:15 -0700

    feat(debug): Added Logout and clear tutorial completion button on JustinTestScreen

--- screens/debug/JustinTestScreen.js
+++ screens/debug/JustinTestScreen.js
@@ -1,11 +1,35 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-
+import { StyleSheet, View, AsyncStorage } from 'react-native';
+import { Image, Button, Text } from 'react-native-elements';
+import AuthContext from '../../contexts/AuthContext';
+import firebase from 'firebase';
+import firebaseObject from '../../config/firebase';
 
 export default function JustinTestScreen() {
+  const { signIn, signOut } = React.useContext(AuthContext);
+
+  firebaseSignOut = async () => {
+    firebase.auth().signOut().then(function() {
+      signOut();
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+
+  clearTutorialCompletion = async () => {
+    await AsyncStorage.removeItem('isFirstLaunch');
+  }
+
   return (
     <View style={styles.container}>
+      <Button
+        title="SIGN OUT"
+        onPress={() => firebaseSignOut()}
+        />
+      <Button
+        title="CLEAR TUTORIAL COMPLETION"
+        onPress={() => clearTutorialCompletion()}
+        />
     </View>
   )
 }
@@ -13,6 +37,8 @@ export default function JustinTestScreen() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
       backgroundColor: '#F5FCFF'
     }
 });
