d5d8c66a9401aa7960ed65c4efa35774167ec68a
Justin Choi <youngyoonc@gmail.com>
2020-04-01 18:22:33 -0700

    Merge branch 'feat/MapView'

--- screens/debug/JayTestScreen.js
+++ screens/debug/JayTestScreen.js
@@ -1,33 +1,53 @@
 import * as React from 'react';
-import { StyleSheet, StatusBar, Platform, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, StatusBar, Platform, View, ScrollView, Dimensions } from 'react-native';
+import { Avatar, Header, Button , Icon} from 'react-native-elements';
 import Constants from 'expo-constants';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
 
-const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
+// Constants
+const SCREEN = Dimensions.get('window');
+const ASPECT_RATIO = SCREEN.width / SCREEN.height;
 const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = 0.0421;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// Below should come from Event Detial in the future
+const meetingLocation = { latitude: 49.2451673, longitude: -122.8933748 }
+const members = [
+  {
+    name: 'Matthew Kim',
+    initial: 'MK',
+    color: '#fc0f03',
+    location : { latitude: 49.3049901, longitude: -122.8332702 },
+  },
+  {
+    name: 'Paul Kim',
+    initial: 'PK',
+    color: '#0362fc',
+    location : { latitude: 49.2620402, longitude: -122.8763948 },
+  },
+  {
+    name: 'Justin Choi',
+    initial: 'JC',
+    color: '#fcba03',
+    location : { latitude: 49.2509886, longitude: -122.8920569 },
+  },
+]
 
 export default class JayTestScreen extends React.Component {
-  
+
   state = {
-    mapRegion: { latitude: 49.2442458, longitude: -122.8926027, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
-    locationResult: null,
-    location: {coords: { latitude: 37.78825, longitude: -122.4324}},
-    juilet: { latitude: 49.2451673, longitude: -122.8933748, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
-    matthew: { latitude: 49.3049901, longitude: -122.8332702, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
-    paul: { latitude: 49.2620402, longitude: -122.8763948, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
-    justin: { latitude: 49.2509886, longitude: -122.8920569, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
+    mapRegion: { latitude: 0, longitude: 0, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA },
   };
-  
 
   componentDidMount() {
     this._getLocationAsync();
   }
-  
-  _handleRegionChange = mapRegion => {
-    this.mapView.animateToRegion(mapRegion);
-  }
+
+  _getLocationAsync = async () => {
+    let location = await Location.getCurrentPositionAsync({});
+    let region = { latitude: location.coords.latitude, longitude: location.coords.longitude, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA }
+    this.setState({ mapRegion: region});
+  };
 
   async _goToMyLocation() {
     let location = await Location.getCurrentPositionAsync({});
@@ -35,126 +55,153 @@ export default class JayTestScreen extends React.Component {
     this.mapView.animateToRegion(region);
   };
 
-  _ZoomOut = ()  => {
-    let coordinates = [
-      { latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude },
-      this.state.juilet,
-      this.state.matthew,
-      this.state.paul,
-      this.state.justin,
-      // to test something far way My Office
-      //{ latitude: 49.2797516, longitude: -123.1235704 },
-    ]
-    this.mapView.fitToCoordinates(coordinates, { edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },animated: true });
-  };
-  
-  _getLocationAsync = async () => {
+  _fitAll = async () => {
     let location = await Location.getCurrentPositionAsync({});
-    this.setState({ locationResult: JSON.stringify(location), location, });
-    let region = { latitude: this.state.location.coords.latitude, longitude: this.state.location.coords.longitude, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA }
-    this.setState({ mapRegion: region});
+    let coordinates = []
+    // user's location
+    coordinates.push({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+    // meeting location
+    coordinates.push(meetingLocation);
+    // members' locations
+    members.map((u) => coordinates.push(u.location))
+    this.mapView.fitToCoordinates(coordinates, { edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },animated: true });
   };
 
   render() {
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <MapView 
+
+        <Header
+          leftComponent={{ icon: 'chevron-left', color: '#fff' }}
+          centerComponent={{ text: 'Map View', style: { color: '#fff' } }}
+          centerContainerStyle= {{flex: 1}}
+          rightComponent={{ text: 'Event Detail', style: { color: '#fff', flexWrap: 'wrap' } }}
+        />
+        {/* MapView */}
+        <MapView
           ref = {(ref)=> this.mapView=ref}
-          style={styles.mapStyle} 
-          showsUserLocation={true}    
-          // below doesnt work with iOS map
-          showsMyLocationButton={true}
-          showsCompass={true}
-          showsPointsOfInterest={true}
+          style={styles.mapStyle}
+          showsUserLocation={true}
           // region : which section of the map to render/zoom
           region={this.state.mapRegion}
         >
-
+          {/* Meeting Location Circle */}
           <MapView.Circle
             center = {{
-              latitude: this.state.juilet.latitude,
-              longitude: this.state.juilet.longitude,
+              latitude: meetingLocation.latitude,
+              longitude: meetingLocation.longitude,
             }}
             radius = {500} // in meters
-            fillColor = { 'rgba(230,238,255,0.5)' }
+            strokeWidth = {2}
+            strokeColor = 'rgba(89, 89, 89, 0.42)'
+            fillColor = 'rgba(89, 89, 89, 0.42)'
           />
-
-          <MapView.Marker
-            //Matthew
-            pinColor = '#fc0f03'
-            coordinate = {{latitude: this.state.matthew.latitude,longitude: this.state.matthew.longitude}}
-          />
-          <MapView.Marker
-            //Paul
-            pinColor = '#0362fc'
-            coordinate = {{latitude: this.state.paul.latitude,longitude: this.state.paul.longitude}}
-          />
-          <MapView.Marker
-            //Justin
-            pinColor = '#fcba03'
-            coordinate = {{latitude: this.state.justin.latitude,longitude: this.state.justin.longitude}}
-          />
-
+          {/* Member Markers */}
+          {
+            members.map((u, i) => {
+              return(
+                <MapView.Marker
+                  key = {i}
+                  pinColor = {u.color}
+                  coordinate = {
+                    {
+                      latitude: u.location.latitude,
+                      longitude: u.location.longitude,
+                    }
+                  }
+                />
+              )
+            })
+          }
         </MapView>
-        
+
+        {/* My Location Button */}
         <View style={styles.myLocationStyle}>
-          <Button title= 'My Location'onPress = {() => this._goToMyLocation()}
+          <Icon
+            reverse
+            name='location-arrow'
+            type='font-awesome'
+            onPress = {() => this._goToMyLocation()}
           />
         </View>
-        <View style={styles.zoomOutStyle}>
-          <Button title= 'Zoom out' onPress = {this._ZoomOut}/>
-        </View>
+
+        {/* Goal In Button */}
         <View style={styles.goalStyle}>
-          <Button title= 'GOAL!'/>
+          <Icon
+            reverse
+            name='check'
+            type='font-awesome'
+          />
         </View>
-        
-        <View style={styles.buttonContainer}>
+
+        <View style={styles.avatarContianer}>
           <ScrollView horizontal={true} contentContainerStyle={{flexGrow: 1, justifyContent : 'center'}}>
-            <Button title= 'Juilet' onPress = {() => this._handleRegionChange(this.state.juilet)}/>
-            <Button title= 'Matthew' onPress = {() => this._handleRegionChange(this.state.matthew)}/>
-            <Button title= 'Paul' onPress = {() => this._handleRegionChange(this.state.paul)}/>
-            <Button title= 'Justin' onPress = {() => this._handleRegionChange(this.state.justin)}/>
+            {/* fitAll */}
+            <View style={styles.avatar}>
+              <Avatar
+                rounded
+                size='medium'
+                icon={{name: 'users', type: 'font-awesome'}}
+                onPress = {() => this._fitAll()}
+              />
+            </View>
+            {/* Meeting Location */}
+            <View style={styles.avatar}>
+              <Avatar
+                rounded
+                size='medium'
+                icon={{name: 'map-marker', type: 'font-awesome'}}
+                onPress = {() => this.mapView.animateToRegion({latitude: meetingLocation.latitude, longitude: meetingLocation.longitude, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA })}
+              />
+            </View>
+            {/* Members */}
+            {
+              members.map((u, i) => {
+                let memberRegion = { latitude: u.location.latitude, longitude: u.location.longitude, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA }
+                console.log(memberRegion)
+                return(
+                  <View style={styles.avatar} key = {i}>
+                    <Avatar
+                      rounded
+                      size='medium'
+                      title = {u.initial}
+                      onPress = {() => this.mapView.animateToRegion(memberRegion)}
+                    />
+                  </View>
+                )
+              })
+            }
           </ScrollView>
         </View>
 
       </View>
     );
   }
-  
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Constants.statusBarHeight,
   },
   mapStyle: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
   },
-  buttonContainer: {
-    bottom: 0,
+  avatarContianer: {
     flexDirection: 'row',
-    position: 'absolute',
     backgroundColor: 'white',
+  },
+  avatar: {
+    margin: 10,
   },
   myLocationStyle: {
     position: 'absolute',
-    top:100,
-    right:0,
-    backgroundColor: 'white',
-  },
-  zoomOutStyle: {
-    position: 'absolute',
     top:150,
     right:0,
-    backgroundColor: 'white',
   },
   goalStyle: {
-    position: 'absolute',
-    bottom:50,
     alignSelf: 'center',
-    backgroundColor: 'white',
+    position: 'absolute',
+    bottom:100,
   },
 });
