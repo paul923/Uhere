6c0f74c44e229ab1db7bc2c776964019cd789784
Justin Choi <youngyoonc@gmail.com>
2020-04-05 21:27:55 -0700

    feat(mysql): created backend server for database connection

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const pool = mysql.createPool({
+  connectionLimit : 5,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_SCHEMA,
+  ssl: "Amazon RDS"
+});
+// Starting our app.
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+// Creating a GET route that returns data from the 'users' table.
+app.get('/retrieve', function (req, res) {
+  // Connecting to the database.
+  pool.getConnection(function (err, connection) {
+    var sql = "SELECT * FROM ??";
+    var parameters = ['test_table'];
+    sql = mysql.format(sql, parameters);
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query(sql, function (error, results, fields) {
+      connection.release();
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+});
+
+app.post('/insert', function (req,res) {
+  // Connecting to the database.
+  pool.getConnection(function (err, connection) {
+    var sql = "INSERT INTO ?? SET ?";
+    var parameters = ['test_table'];
+    sql = mysql.format(sql, parameters);
+    // Executing the MySQL query (select all data from the 'users' table).
+    connection.query(sql, req.body, function (error, results, fields) {
+      connection.release();
+      // If some error occurs, we throw an error.
+      if (error) throw error;
+
+      // Getting the 'response' from the database and sending it to our route. This is were the data is.
+      res.send(results)
+    });
+  });
+})
+
+// Starting our server.
+app.listen(3000, () => {
+ console.log('Listening on Port 3000');
+});
