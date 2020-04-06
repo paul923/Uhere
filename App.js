2614d17b5ed1f1ef1153f1389b064ef1bbcd1455
Justin Choi <youngyoonc@gmail.com>
2020-04-05 19:43:01 -0700

    fix: Fixed issue with login loading by adding 1 second timeout after retrieving token

--- App.js
+++ App.js
@@ -170,6 +170,7 @@ export default function App(props) {
       try {
         AsyncStorage.getItem('userToken').then(userToken => {
           dispatch({ type: 'RESTORE_TOKEN', token: userToken })
+          setTimeout(() => dispatch({ type: 'HIDE_LOADING_SCREEN'}), 1000);
         });
       } catch (e) {
         // Restoring token failed
@@ -223,7 +224,6 @@ export default function App(props) {
       checkIfSkip();
       checkIfFirstLaunchedAsync();
       restoreUserTokenAsync();
-      dispatch({ type: 'HIDE_LOADING_SCREEN'})
     }
     loadAsyncData();
     loadFontAsync();
@@ -260,7 +260,7 @@ export default function App(props) {
         <AuthContext.Provider value={authContext}>
           <Stack.Navigator
             headerMode="none">
-            {state.userToken == null ? (
+            {state.userToken == null && state.showLoadingScreen ? (
               <Stack.Screen name="LoginNavigator" component={LoginNavigator} />
             ) : (
                 !state.skipProfile ? (
