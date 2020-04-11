0c0932960861d1f31d148bb2195260b7ae49bc48
Paul Kim <paulgmkim@gmail.com>
2020-04-11 15:08:57 -0700

    feat: eventdetail sidebar layout created. Needs to confirm on UI and design and then more styling

--- screens/debug/DrawerLayoutScreen.js
+++ screens/debug/DrawerLayoutScreen.js
@@ -49,8 +49,56 @@ export default function DrawerLayoutScreen() {
 
 function menuContent(){
   return(
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.sideMenu}>
+      <View style={{flex: 10}}>
+        <View style={styles.hostContainer}>
+          <Text>Host</Text>
+          <Avatar
+            size='large'
+            source={{uri: 'https://www.collinsdictionary.com/images/full/rose_277351964.jpg'}}
+            avatarStyle={{borderWidth: 2, borderRadius: 5, borderColor: 'red'}}
+          />
+          <Text>Host Name</Text>
+        </View>
+
+        <View style={styles.friendsContainer}>
+          <Text>Friends</Text>
+          <View style={styles.friendsButton}>
+            <Button
+              title="Invite"
+              icon={{
+                name: "pluscircleo",
+                type: "antdesign"
+              }}
+              type="outline"
+              containerStyle={{flex: 1, marginHorizontal: 3,}}
+            />
+            <Button
+              title="Edit"
+              icon={{
+                name: "minuscircleo",
+                type: "antdesign"
+              }}
+              type="outline"
+              containerStyle={{flex: 1, marginHorizontal: 3}}
+            />
+          </View>
+          <Text>Flatlist of Friends</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.bottomBar}>
+        <Icon
+          name="md-exit"
+          type="ionicon"
+          iconStyle={styles.bottomIcon}
+        />
+        <Icon
+          name="md-notifications"
+          type="ionicon"
+        />
+      </View>
     </View>
   )
 }
@@ -64,5 +112,32 @@ const styles = StyleSheet.create({
     button: {
       margin: 5,
       height: 50
+    },
+    sideMenu: {
+      flex: 1,
+      backgroundColor: 'white',
+      borderWidth: 1,
+    },
+    hostContainer: {
+      flex: 2,
+      borderWidth: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    friendsContainer: {
+      flex:4,
+      borderWidth: 1,
+      padding: 5
+    },
+    bottomBar: {
+      borderWidth: 1,
+      flexDirection: 'row',
+      padding: 5,
+    },
+    friendsButton: {
+      flexDirection: 'row'
+    },
+    bottomIcon:{
+      marginHorizontal: 10
     }
 });
