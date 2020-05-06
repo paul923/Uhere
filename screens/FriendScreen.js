ebb73f8ee37d4b5851ccc29702d0a860a0058330
Paul Kim <paulgmkim@gmail.com>
2020-05-05 19:50:52 -0700

    feat: converted friendscreen class (component) into functional component

--- screens/FriendScreen.js
+++ screens/FriendScreen.js
@@ -9,15 +9,22 @@ import { SimpleAnimation } from 'react-native-simple-animations';
 import { TouchableOpacity} from 'react-native-gesture-handler'
 
 
-export default class FriendScreen extends React.Component {
-  state = {
-    searchText: "",
-    data: [],
-    filteredData: [],
-    dropDownToggle: false
-  };
 
-  retrieveFriend = async () => {
+
+export default function FriendScreen({navigation}) {
+  const [ searchText, setSearchText] = React.useState("");
+  const [ friends, setFriends] = React.useState([]);
+  const [ filteredData, setFilteredData] = React.useState([]);
+  const [ selectedFriends, setSelectedFriends] = React.useState([]);
+  const [ dropDownToggle, setDropDownToggle] = React.useState(false);
+
+  React.useEffect(() => {
+    retrieveFriend();
+    // Sorts friends list on initial load
+
+  }, []);
+
+  async function retrieveFriend() {
     let response = await fetch(`http://${backend}:3000/relationship/${firebase.auth().currentUser.uid}`, {
       method: 'GET',
       headers: {
@@ -27,166 +34,179 @@ export default class FriendScreen extends React.Component {
     });
     let responseJson = await response.json();
     responseJson.response.sort((a,b) => a.Nickname.localeCompare(b.Nickname));
-    this.setState({data: responseJson.response});
+    setFriends(responseJson.response);
   }
-  componentDidMount(){
-    this.retrieveFriend();
-  }
-
-
-
-  search = (searchText) => {
-    this.setState({searchText: searchText});
-
-    let filteredData = this.state.data.filter(function (item) {
-      return item.Nickname.toLowerCase().includes(searchText.toLowerCase()) || item.Username.toLowerCase().includes(searchText.toLowerCase())
-    });
-
-    this.setState({filteredData: filteredData})
-  }
-
-
-
-
-  renderFriendsCard = ({ item }) => (
+  
+  function renderFriendsCard({ item }){
+    return (
     <FriendCard
       avatarUrl= {item.AvatarURI}
       avatarTitle= {!item.AvatarURI && item.Nickname.substr(0, 2).toUpperCase()}
       displayName = {item.Nickname}
       userId = {item.Username}
+      checkBox={{
+        size: 35,
+        checkedIcon: 'dot-circle-o',
+        uncheckedIcon: 'circle-o',
+        checkedColor:'#ff8a8a',
+        uncheckedColor: '#ff8a8a',
+        checked: selectedFriends.includes(item),
+        onPress: () => selectFriend(item)
+      }}
     />
-  )
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header
-          containerStyle={{zIndex:200}}
-          centerComponent={{ text: 'FRIENDS', style: { color: '#fff', fontSize: 20 } }}
-          statusBarProps={{translucent: true}}
-          rightComponent={
-            <Icon
-              name="user-plus"
-              type="feather"
-              color="white"
-              underlayColor="transparent"
-              onPress = {()=> {this.setState({dropDownToggle: !this.state.dropDownToggle})}}
-            />
-          }
-        />
-
-        {// Drop down menu when add user buttton clicked on header
-        this.state.dropDownToggle && (
-          <SimpleAnimation
-            duration={500}
-            distance={50}
-            movementType="slide"
-            direction="down"
-            fade
-            aim="in"
-            style={{zIndex:100}}>
-            <View
-              style={{
-                height: 120,
-                backgroundColor: 'white',
-                position: 'absolute',
-                width: '100%',
-                flex:1,
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
-                alignItems: 'center'
-            }}>
-              <TouchableOpacity onPress={()=> {this.setState({dropDownToggle: false});}}>
-                <View style={styles.dropDownButton}>
-                  <Icon name="edit" type="entypo"/>
-                  <Text style={styles.dropDownButtonText}>Edit Friend</Text>
-                </View>
-              </TouchableOpacity>
-              <View style={{height: '70%', borderWidth: 0.5, borderColor: '#EBEBEB'}}></View>
-              <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Add Friend Selection');this.setState({dropDownToggle: false});}}>
-                <View style={styles.dropDownButton}>
-                  <Icon name="user" type="feather"/>
-                  <Text style={styles.dropDownButtonText}>Add Friend</Text>
-                </View>
-              </TouchableOpacity>
-              <View style={{height: '70%', borderWidth: 0.5, borderColor: '#EBEBEB'}}></View>
-              <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Create Group');this.setState({dropDownToggle: false});}}>
-                <View style={styles.dropDownButton}>
-                  <Icon name="users" type="feather"/>
-                  <Text style={styles.dropDownButtonText}>Create Group</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          </SimpleAnimation>
-          )
-        }
-        {// greyed out background when dropdown is toggled
-          this.state.dropDownToggle && (
-            <TouchableWithoutFeedback onPress={()=> {console.log("test");this.setState({dropDownToggle: false})}}>
-            <View style={{
-              flex: 1,
-              zIndex: 20,
-              height: "100%",
-              width: '100%',
-              position: 'absolute',
-              backgroundColor: this.state.dropDownToggle ? 'black' : null,
-              opacity: this.state.dropDownToggle ? 0.7 : null,
-            }}>
-            </View>
-            </TouchableWithoutFeedback>
-          )
-        }
-
-        <View style={{
-          flex: 1,
-          zIndex: 10
-        }}>
-          <SearchBar
-            round={true}
-            lightTheme={true}
-            placeholder="Search..."
-            autoCapitalize='none'
-            autoCorrect={false}
-            onChangeText={this.search}
-            value={this.state.searchText}
-            containerStyle={{
-              backgroundColor:"white",
-              margin: 10,
-              borderColor: "#C4C4C4",
-              borderWidth: 1,
-              borderRadius: 10,
-              padding: 3
-            }}
-            inputContainerStyle={{
-              backgroundColor:"white"
-            }}
-            inputStyle={{
-              backgroundColor:"white"
-            }}
-            leftIconContainerStyle={{
-              backgroundColor:"white"
-            }}
-            rightIconContainerStyle={{
-              backgroundColor:"white"
-            }}
-          />
-
-          <FlatList
-            data={this.state.filteredData && this.state.filteredData.length > 0 ? this.state.filteredData : (this.state.searchText.length === 0 && this.state.data)}
-            renderItem={this.renderFriendsCard}
-            keyExtractor={(item) => item.Username}
-            contentContainerStyle={{
-              paddingLeft: 20,
-              paddingRight: 20,
-              backgroundColor: "white"
-            }}
-            bounces={false}
-          />
-        </View>
-      </View>
     )
   }
+  
+  function selectFriend (item) {
+    console.log(item);
+    if(!selectedFriends.includes(item)){
+      setSelectedFriends([...selectedFriends, item])
+    } else {
+      setSelectedFriends(selectedFriends.filter(a => a !== item));
+    }
+    console.log(selectedFriends)
+  }
+
+
+
+  function friendSearch(text) {
+    setSearchText(text);
+  
+    let filtered = friends.filter(function (item) {
+      return item.Nickname.toLowerCase().includes(text.toLowerCase()) || item.Username.toLowerCase().includes(text.toLowerCase())
+    });
+  
+    setFilteredData(filtered)
+  }
+
+   
+  return (
+    <View style={styles.container}>
+      <Header
+        containerStyle={{zIndex:200}}
+        centerComponent={{ text: 'FRIENDS', style: { color: '#fff', fontSize: 20 } }}
+        statusBarProps={{translucent: true}}
+        rightComponent={
+          <Icon
+            name="user-plus"
+            type="feather"
+            color="white"
+            underlayColor="transparent"
+            onPress = {()=> {setDropDownToggle(!dropDownToggle)}}
+          />
+        }
+      />
+
+      {// Drop down menu when add user buttton clicked on header
+      dropDownToggle && (
+        <SimpleAnimation
+          duration={500}
+          distance={50}
+          movementType="slide"
+          direction="down"
+          fade
+          aim="in"
+          style={{zIndex:100}}>
+          <View
+            style={{
+              height: 120,
+              backgroundColor: 'white',
+              position: 'absolute',
+              width: '100%',
+              flex:1,
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              alignItems: 'center'
+          }}>
+            <TouchableOpacity onPress={()=> setDropDownToggle(false)}>
+              <View style={styles.dropDownButton}>
+                <Icon name="edit" type="entypo"/>
+                <Text style={styles.dropDownButtonText}>Edit Friend</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={{height: '70%', borderWidth: 0.5, borderColor: '#EBEBEB'}}></View>
+            <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Add Friend Selection');setDropDownToggle(false)}}>
+              <View style={styles.dropDownButton}>
+                <Icon name="user" type="feather"/>
+                <Text style={styles.dropDownButtonText}>Add Friend</Text>
+              </View>
+            </TouchableOpacity>
+            <View style={{height: '70%', borderWidth: 0.5, borderColor: '#EBEBEB'}}></View>
+            <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Create Group');setDropDownToggle(false);}}>
+              <View style={styles.dropDownButton}>
+                <Icon name="users" type="feather"/>
+                <Text style={styles.dropDownButtonText}>Create Group</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </SimpleAnimation>
+        )
+      }
+      {// greyed out background when dropdown is toggled
+        dropDownToggle && (
+          <TouchableWithoutFeedback onPress={()=> {console.log("test");setDropDownToggle(false)}}>
+          <View style={{
+            flex: 1,
+            zIndex: 20,
+            height: "100%",
+            width: '100%',
+            position: 'absolute',
+            backgroundColor: dropDownToggle ? 'black' : null,
+            opacity: dropDownToggle ? 0.7 : null,
+          }}>
+          </View>
+          </TouchableWithoutFeedback>
+        )
+      }
+
+      <View style={{
+        flex: 1,
+        zIndex: 10
+      }}>
+        <SearchBar
+          round={true}
+          lightTheme={true}
+          placeholder="Search..."
+          autoCapitalize='none'
+          autoCorrect={false}
+          onChangeText={friendSearch}
+          value={searchText}
+          containerStyle={{
+            backgroundColor:"white",
+            margin: 10,
+            borderColor: "#C4C4C4",
+            borderWidth: 1,
+            borderRadius: 10,
+            padding: 3
+          }}
+          inputContainerStyle={{
+            backgroundColor:"white"
+          }}
+          inputStyle={{
+            backgroundColor:"white"
+          }}
+          leftIconContainerStyle={{
+            backgroundColor:"white"
+          }}
+          rightIconContainerStyle={{
+            backgroundColor:"white"
+          }}
+        />
+
+        <FlatList
+          data={filteredData && filteredData.length > 0 ? filteredData : (searchText.length === 0 && friends)}
+          renderItem={renderFriendsCard}
+          keyExtractor={(item) => item.UserId}
+          contentContainerStyle={{
+            paddingLeft: 20,
+            paddingRight: 20,
+            backgroundColor: "white"
+          }}
+          bounces={false}
+        />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
